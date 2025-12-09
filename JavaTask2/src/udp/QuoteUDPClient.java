845bfad597963e4e000e295b834c085beb0acc87
roythetool <roymaman222@gmail.com>
2025-12-09 12:27:45 +0200

    finishing client and editing server to adjust client

--- JavaTask2/src/udp/QuoteUDPClient.java
+++ JavaTask2/src/udp/QuoteUDPClient.java
@@ -1,10 +1,61 @@
 package udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class QuoteUDPClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		DatagramSocket socket = null;
+		int port = 8080;
 
+		try {
+			// creating client
+			socket = new DatagramSocket();
+			InetAddress serverAddress = InetAddress.getByName("localhost");
+
+			while (true) {
+//		get user's text
+				System.out.print("Enter 'GET' for quote or 'exit' to quit: ");
+				String sc = scanner.nextLine();
+
+//				creating packet for user text and sending to server
+				byte[] sendBuffer = sc.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, port);
+				socket.send(sendPacket);
+
+//				in case that user text is exit
+				if (sc.trim().equalsIgnoreCase("exit")) {
+					System.out.println("client finished");
+					break;
+				}
+
+//				waiting for server to give us response
+				byte[] receiveBuffer = new byte[1024];
+				DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+
+				socket.receive(receivePacket);
+
+				// converting resonsponse into string
+				String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+				// handling 2 cases of response quote or error
+				if (serverResponse.startsWith("Error")) {
+					System.out.println(serverResponse + "\n");
+				} else {
+					System.out.println("Quote received: " + serverResponse + "\n");
+				}
+			}
+
+			socket.close();
+			scanner.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
--- JavaTask2/src/udp/QuoteUDPServer.java
+++ JavaTask2/src/udp/QuoteUDPServer.java
@@ -10,10 +10,11 @@ public class QuoteUDPServer {
 	public static void main(String[] args) {
 		// רשימת הציטוטים שלך
 		String[] quotes = { "hey user", "hey roy", "hey shalev", "game over", "start game" };
+		int port = 8080;
 
 		try {
 
-			DatagramSocket socket = new DatagramSocket(8080);
+			DatagramSocket socket = new DatagramSocket(port);
 			System.out.println("Server is running on port 8080...");
 
 			byte[] receiveBuffer = new byte[1024];
@@ -25,7 +26,7 @@ public class QuoteUDPServer {
 				socket.receive(receivePacket);
 
 				String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
-				System.out.println("Received request: " + clientMessage);
+				System.out.println("Request received : " + clientMessage);
 
 				// saving client's crucial details to send beck to him (port, address)
 				InetAddress clientAddress = receivePacket.getAddress();
@@ -33,10 +34,9 @@ public class QuoteUDPServer {
 
 				// building three cases for each potentially behavior of the app
 				try {
-					// 1. user sends 1
+					// 1. user sends exit
 					if (clientMessage.trim().equalsIgnoreCase("exit")) {
-						System.out.println("Server stopping...");
-						break; // יציאה מהלולאה -> ממשיך לשורת ה-close למטה
+						break;
 					}
 
 					// 2. user send GET
@@ -56,20 +56,18 @@ public class QuoteUDPServer {
 					DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress,
 							clientPort);
 					socket.send(sendPacket);
-					System.out.println("Sent quote to client.");
 
 				} catch (Exception e) {
 					// sending to client the exception
-					String errorMessage = "Error from Server: " + e.getMessage();
+					String errorMessage = "Error: " + e.getMessage();
 					sendBuffer = errorMessage.getBytes();
 					DatagramPacket errorPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress,
 							clientPort);
 					socket.send(errorPacket);
 				}
 			}
-
 			socket.close();
-			System.out.println("Socket closed.");
+			System.out.println("Server shutting down. ");
 
 		} catch (Exception e) {
 			// תופס שגיאות כלליות של השרת (למשל אם הפורט תפוס)
