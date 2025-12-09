6f214dd9141586a156ee28b5c5cd2adb0509cdfb
roythetool <roymaman222@gmail.com>
2025-12-09 12:00:40 +0200

    finishing server

--- JavaTask2/src/udp/QuoteUDPServer.java
+++ JavaTask2/src/udp/QuoteUDPServer.java
@@ -1,10 +1,79 @@
 package udp;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Random;
+
 public class QuoteUDPServer {
 
 	public static void main(String[] args) {
+		// רשימת הציטוטים שלך
+		String[] quotes = { "hey user", "hey roy", "hey shalev", "game over", "start game" };
 
-		int shalev = 8;
+		try {
+
+			DatagramSocket socket = new DatagramSocket(8080);
+			System.out.println("Server is running on port 8080...");
+
+			byte[] receiveBuffer = new byte[1024];
+			byte[] sendBuffer;
+
+			while (true) {
+				// waiting for client message and reading the message
+				DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+				socket.receive(receivePacket);
+
+				String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+				System.out.println("Received request: " + clientMessage);
+
+				// saving client's crucial details to send beck to him (port, address)
+				InetAddress clientAddress = receivePacket.getAddress();
+				int clientPort = receivePacket.getPort();
+
+				// building three cases for each potentially behavior of the app
+				try {
+					// 1. user sends 1
+					if (clientMessage.trim().equalsIgnoreCase("exit")) {
+						System.out.println("Server stopping...");
+						break; // יציאה מהלולאה -> ממשיך לשורת ה-close למטה
+					}
+
+					// 2. user send GET
+					String response;
+
+					if (clientMessage.trim().equalsIgnoreCase("GET")) {
+						Random random = new Random();
+						int randomIndex = random.nextInt(quotes.length);
+						response = quotes[randomIndex];
+					} else {
+						// 3. user sends something else = exceptionז
+						throw new Exception("Unknown command. Please send 'GET' or 'exit'.");
+					}
+
+					// send quote to client
+					sendBuffer = response.getBytes();
+					DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress,
+							clientPort);
+					socket.send(sendPacket);
+					System.out.println("Sent quote to client.");
+
+				} catch (Exception e) {
+					// sending to client the exception
+					String errorMessage = "Error from Server: " + e.getMessage();
+					sendBuffer = errorMessage.getBytes();
+					DatagramPacket errorPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress,
+							clientPort);
+					socket.send(errorPacket);
+				}
+			}
+
+			socket.close();
+			System.out.println("Socket closed.");
+
+		} catch (Exception e) {
+			// תופס שגיאות כלליות של השרת (למשל אם הפורט תפוס)
+			e.printStackTrace();
+		}
 	}
-
 }
